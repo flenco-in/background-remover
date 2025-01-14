578bb4d973a4e1bbb3142246cff30569aecbbac1
Atish Paul <atishpaul01@gmail.com>
2025-01-14 19:58:58 +0530

    Update selenium_utils.py

--- selenium_utils.py
+++ selenium_utils.py
@@ -31,7 +31,7 @@ class SeleniumImageGenerator:
         Configure Chrome WebDriver with optimized settings
         """
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
